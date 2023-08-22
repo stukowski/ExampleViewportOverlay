98deb9b7c31b0485144b9110c9c5f285ff5896ec
Alexander Stukowski <stukowski@users.noreply.github.com>
2023-08-22 10:47:52 +0000

    Added class interface implementation

--- src/ExampleViewportOverlay/__init__.py
+++ src/ExampleViewportOverlay/__init__.py
@@ -2,6 +2,7 @@
 # An example for a Python-based viewport layer.
 
 from ovito.vis import *
+from traits.api import *
 
 def example_render_function(args: PythonViewportOverlay.Arguments):
     
@@ -16,3 +17,21 @@ def example_render_function(args: PythonViewportOverlay.Arguments):
         num_particles = data.particles.count if data.particles else 0
         text2 = f"{num_particles} particles"
         args.painter.drawText(10, args.painter.window().height() - 10, text2)
+
+class MyExampleOverlay(ViewportOverlayInterface):
+
+    myparam = Int(0)
+
+    def render(self, args: PythonViewportOverlay.Arguments, **kwargs):
+
+        # This demo code prints the current animation frame into the upper left corner of the viewport.
+        text1 = f"Frame {args.frame}"
+        args.painter.drawText(10, 10 + args.painter.fontMetrics().ascent(), text1)
+
+        # Also print the current number of particles into the lower left corner of the viewport.
+        pipeline = args.scene.selected_pipeline
+        if pipeline:
+            data = pipeline.compute(args.frame)
+            num_particles = data.particles.count if data.particles else 0
+            text2 = f"{num_particles} particles"
+            args.painter.drawText(10, args.painter.window().height() - 10, text2)
